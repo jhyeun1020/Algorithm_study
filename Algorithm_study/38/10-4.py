3ca0d3e973172afbfb22a0cbcacce3f758098f64
Junghyun <jhyeun1020@naver.com>
2021-02-08 20:14:55 +0900

    스터디 38일차(재귀)

---
+++ Algorithm_study/38/10-4.py
@@ -0,0 +1,14 @@
+N = int(input())
+def hanoi(N,a,b,c):
+    if N == 1:
+        print("n: %d, a: %d, c: %d"%(N,a,c))
+    else:
+        print("n: %d,a: %d,c: %d"%(N,a,c))
+        hanoi(N - 1, a, c, b)
+        print(a, c)
+        hanoi(N - 1, b, a, c)
+sum = 1
+for i in range(N-1):
+    sum = sum*2 + 1
+print(sum)
+hanoi(N,1,2,3)
