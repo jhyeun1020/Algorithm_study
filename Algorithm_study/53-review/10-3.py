356d58f366c137dd4521980c3824710779d42c9f
Kim Junghyun <jhyeun1020@naver.com>
2021-03-02 22:10:12 +0900

    git desktop test 2021-03-02

--- Algorithm_study/53-review/10-3.py
+++ Algorithm_study/53-review/10-3.py
@@ -1,1 +1,17 @@
-print("test")
+N = int(input())
+square = [[0 for _ in range(N)] for _ in range(N)]
+def star(x):
+    global square
+    if x == 3:
+        square[0][:3] = square[2][:3] = [1]*3
+        square[1][:3] = [1,0,1]
+        return
+star(N)
+for i in square:
+    for j in i:
+        if j:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
+# 다시 다시 다시 다시 다시 다시 다시 다시 다시 다시
